bcf4caacb8ce042251f3bc7271237f5f24aa0305
ifarbie <farizrifkyberliano@gmail.com>
2025-01-25 16:09:51 +0700

    [update] updated default setup app

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import './styles/style.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import NoteApp from './compo nents/NoteApp';
+import NoteApp from './components/NoteApp';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<NoteApp />);
